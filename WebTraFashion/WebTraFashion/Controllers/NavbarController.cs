2480b788130305fdf4a1aec75ceacdea1ca0a483
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-04-29 23:06:01 +0700

    New Web

---
+++ WebTraFashion/WebTraFashion/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using WebTraFashion.Domain;
+using WebTraFashion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTraFashion.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
---
+++ WebTraFashion/WebTraFashion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTraFashion.MvcApplication" Language="C#" %>
